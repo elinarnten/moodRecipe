6d5a74af00f1bca26d732627edd962afc192c487
ROSANNA PISTONE <rosanna@pistone.se>
2022-02-24 10:35:10 +0100

    Add state for input value and that the value's saved for other components to handle

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,20 +1,18 @@
+import { useState } from "react";
 import { render } from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./header";
 import StartPage from "./startPage";
 
 function Layout() {
-  //const [inputNameValue, setInputNameValue] = useState('');
-  //console.log(inputNameValue)
+  const [inputNameValue, setInputNameValue] = useState('');
+  console.log(inputNameValue);
   return( 
-  <>
+    <>
   <Header />
-  <StartPage /> 
+  <StartPage nameInput={setInputNameValue}/> 
   </>
 )
 }
 
 export default Layout;
-
-
-///* setNameInput={inputNameValue} */
--- src/components/startPage.tsx
+++ src/components/startPage.tsx
@@ -1,31 +1,38 @@
 import React from "react";
 import { CSSProperties } from "react";
 
-function StartPage() {
+function StartPage(props: any) {
   return (
-    <div style={divContent}>
-        <h2>Welcome to RÖMUH!</h2>
-        <p>Sometimes we feel what we deserve - sometimes not. RÖMUH helps you to embrace how you feel 
-            here and now: in this moment. The purpose isn't to make you happy, here all moods are valued 
-            and it's important to be in different states. In other words.. if you're angry you should be 
-            allowed to be angry, if you're happy you should be allowed to be happy.
-        </p>
-        <h3>Let's start, what's your name?</h3>
-        <input style={inputStyle} /* onChange={newInput => props.setNameInput(newInput)} */></input>
-        <button style={buttonStyle}>Go!</button>
-    </div>
+    <div style={divContainer}> 
+        <div style={divContent}>
+            <h2>Welcome to RÖMUH!</h2>
+            <p>Sometimes we feel what we deserve - sometimes not. RÖMUH helps you to embrace how you feel 
+                here and now: in this moment. The purpose isn't to make you happy, here all moods are valued 
+                and it's important to be in different states. In other words.. if you're angry you should be 
+                allowed to be angry, if you're happy you should be allowed to be happy.
+            </p>
+            <h3>Let's start, what's your name?</h3>
+            <input style={inputStyle} onChange={newInput => props.nameInput(newInput.target.value)}></input>
+            <button style={buttonStyle}>Go!</button>
+        </div>
+    </div> 
   );
+}
+const divContainer: CSSProperties = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
 }
 
 const divContent: CSSProperties = {
   display: "flex",
   flexDirection: "column",
-  alignContent: "center",
   alignItems: "center",
   textAlign: "center",
-  padding: "6rem",
+  marginTop: "4rem",
   color: "#333",
   width: "20rem",
+  
 };
 
 const inputStyle: CSSProperties = {
@@ -46,6 +53,5 @@ const buttonStyle: CSSProperties = {
     color: "#333",
     margin: "1rem"
 };
-
 
 export default StartPage;
