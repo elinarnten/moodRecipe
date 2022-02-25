96f4a3fd0dfa9d122638f807c4b247c6bd898177
Jannie87 <jannie.backman@gmail.com>
2022-02-25 14:24:44 +0100

    removed moodboard

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,19 +3,25 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Layout from "./layout";
 import MoodQuestion from "./moodQuestion";
-import MoodBoard from "./moodBoard";
+
+import Happy from "./happy";
+import Sad from "./sad";
+import Angry from "./angry";
+import Bored from "./bored";
 
 function App() {
   return (
-  <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />} />
-          <Route path='/moodQuestion' element={<MoodQuestion />}/>
-          <Route path='/moodBoard' element={<MoodBoard />}/>
-        </Routes>
-  </BrowserRouter>
-    
-    );
-  }
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />} />
+        <Route path="/moodQuestion" element={<MoodQuestion />} />
+        <Route path="/happy" element={<Happy />} />
+        <Route path="/sad" element={<Sad />} />
+        <Route path="/angry" element={<Angry />} />
+        <Route path="/bored" element={<Bored />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 export default App;
