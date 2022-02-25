761ea6e0ce0e5e12bb68526da1eb8087b802936f
Jannie87 <jannie.backman@gmail.com>
2022-02-25 09:58:10 +0100

    Added  food and movie

--- src/components/moodBoard.tsx
+++ src/components/moodBoard.tsx
@@ -1,13 +1,30 @@
 import { CSSProperties } from "react";
 import Happy from "../assets/happy.png";
 
-
 function MoodBoard() {
   return (
     <div style={containerStyle}>
       <div style={contentStyle}>
         <img src={Happy} />
         <h1>You feel happy!</h1>
+        <div style={{ display: "flex", gap: "2rem" }}>
+          <div style={{ gap: "2rem" }}>
+            <div className="foodOne">Food 1</div>
+            <div className="foodTwo">Food 2 </div>
+            <div className="foodThree">Food 3 </div>
+          </div>
+
+          <div>
+            <div className="moviePic" style={boxStyle} />
+            <div className="moviePic" style={boxStyle} />
+            <div className="moviePic" style={boxStyle} />
+          </div>
+          <div>
+            <div className="movieOne">Movie 1</div>
+            <div className="movieTwo">Movie 2</div>
+            <div className="movieThree">Movie 3</div>
+          </div>
+        </div>
       </div>
     </div>
   );
@@ -29,6 +46,13 @@ const contentStyle: CSSProperties = {
   display: "flex",
   flexDirection: "column",
   marginTop: "2.5rem",
+};
+
+const boxStyle: CSSProperties = {
+  backgroundColor: "lightgrey",
+  height: "2rem",
+  width: "2rem",
+  marginBottom: "1rem",
 };
 
 export default MoodBoard;
