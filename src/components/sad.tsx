71dde2c59f25ef9c15056708b07eeb392a2efac0
ROSANNA PISTONE <rosanna@pistone.se>
2022-02-28 15:32:54 +0100

    Changed layout

--- src/components/sad.tsx
+++ src/components/sad.tsx
@@ -3,6 +3,9 @@ import Intern from "../assets/theIntern.jpg";
 import Holiday from "../assets/theholiday_cover.jpg";
 import Enchanto from "../assets/enchanto_cover.jpg";
 import sad from "../assets/sad.png";
+import sadDonut from "../assets/sad-donut.jpg";
+import sadIceCream from "../assets/sad-ice-cream.jpg";
+import sadPopcorn from "../assets/sad-popcorn.jpg";
 
 function Sad() {
   return (
@@ -10,27 +13,48 @@ function Sad() {
       <div style={contentStyle}>
         <img src={sad} alt="sadEmoji" />
         <h1>You feel sad!</h1>
-        <div style={{ display: "flex", gap: "2rem" }}>
-          <div style={{ paddingTop: "1rem" }}>
-            <span style={textStyle}> Food that matches Your mood:</span>
-            <div className="foodOne" style={foodMovieStyle}>
-              Comfortfood
+        <div style={{ display: "flex", gap: "1rem", flexDirection: "column" }}>
+          <span style={textStyle}> Food that matches your mood:</span>
+
+          <div style={contentBlock}>
+            <div style={foodBlock}>
+              <div style={foodOne}>
+                <h4>Donut</h4>
+                <img src={sadDonut} alt="moviecover" style={coverStyle} />
+              </div>
+              <div style={foodTwo}>
+                <h4>Ice Cream</h4>
+                <img src={sadIceCream} alt="moviecover" style={coverStyle} />
+              </div>
+              <div style={foodThree}>
+                <h4>Popcorn</h4>
+                <img src={sadPopcorn} alt="moviecover" style={coverStyle} />
+              </div>
             </div>
-            <div className="foodTwo" style={foodMovieStyle}>
-              Glass
-            </div>
-            <div className="foodThree" style={foodMovieStyle}>
-              Choklad
+
+            <div style={movieBlock}>
+              <div style={movieOne}>
+                <img src={Intern} alt="moviecover" style={coverStyle} />
+                <h4>The Intern</h4>
+              </div>
+              <div style={movieTwo}>
+                <img src={Holiday} alt="moviecover" style={coverStyle} />
+                <h4>The Holiday</h4>
+              </div>
+              <div style={movieThree}>
+                <img src={Enchanto} alt="moviecover" style={coverStyle} />
+                <h4>Enchanto</h4>
+              </div>
             </div>
           </div>
 
-          <div style={{ padding: "1rem", marginTop: "2rem" }}>
+          {/*  <div style={{ padding: "1rem", marginTop: "2rem" }}>
             <img src={Intern} alt="moviecover" style={coverStyle} />
             <img src={Holiday} alt="moviecover" style={coverStyle} />
             <img src={Enchanto} alt="moviecover" style={coverStyle} />
           </div>
           <div style={{ paddingTop: "1rem" }}>
-            <span style={textStyle}>Movies that matches Your mood:</span>
+            <span style={textStyle}>Movies that matches your mood:</span>
             <div className="movieOne" style={foodMovieStyle}>
               The Holiday
             </div>
@@ -40,12 +64,67 @@ function Sad() {
             <div className="movieThree" style={foodMovieStyle}>
               The Intern
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
   );
 }
+
+const contentBlock: CSSProperties = {
+  display: "flex",
+};
+
+const foodBlock: CSSProperties = {
+  display: "flex",
+  flexDirection: "column",
+  gap: ".5rem",
+  alignItems: "flex-end",
+};
+
+const foodOne: CSSProperties = {
+  display: "flex",
+  flexDirection: "row",
+  alignItems: "center",
+};
+
+const foodTwo: CSSProperties = {
+  display: "flex",
+  flexDirection: "row",
+  alignItems: "center",
+};
+
+const foodThree: CSSProperties = {
+  display: "flex",
+  flexDirection: "row",
+  alignItems: "center",
+};
+
+const movieBlock: CSSProperties = {
+  display: "flex",
+  flexDirection: "column",
+  gap: ".5rem",
+  alignItems: "flex-start",
+};
+
+const movieOne: CSSProperties = {
+  display: "flex",
+  flexDirection: "row",
+  alignItems: "center",
+};
+
+const movieTwo: CSSProperties = {
+  display: "flex",
+  flexDirection: "row",
+  alignItems: "center",
+};
+
+const movieThree: CSSProperties = {
+  display: "flex",
+  flexDirection: "row",
+  alignItems: "center",
+};
+
 const containerStyle: CSSProperties = {
   display: "flex",
   justifyContent: "center",
@@ -69,7 +148,7 @@ const textStyle: CSSProperties = {
 };
 
 const foodMovieStyle: CSSProperties = {
-  paddingTop: "1.5rem",
+  paddingTop: "1rem",
 };
 const coverStyle: CSSProperties = {
   width: "80px",
